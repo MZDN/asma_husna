be7721758a02285e922e9b0c3aee46769dc9fb44
MZ <m4zdn@outlook.com>
2021-02-01 22:25:01 +0100

    audio and search function added

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/moctarz/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/moctarz/Desktop/devs/2021.1/flutter/demo/asma_husna"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/moctarz/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:google_fonts_arabic/fonts.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 Future<Names> fetchNames() async {
   final response = await http.get(
       'https://raw.githubusercontent.com/MZDN/asma-u-llahi-l-husna/main/BeautifulNamesOfAllah.json');
 
-  if (response.statusCode == 200) {
+  if (response.statusCode == 200 && json.decode(response.body) != null) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
     return Names.fromJson(jsonDecode(response.body)["AsmaHusna"] as List);
@@ -19,6 +21,7 @@ Future<Names> fetchNames() async {
     throw Exception('Failed to load Names');
   }
 }
+//https://stackoverflow.com/questions/51360481/how-can-i-to-load-and-query-local-json-data-in-flutter-mobile-app
 
 class Names {
   final List<Name> names;
@@ -56,6 +59,7 @@ void main() => runApp(AsmsHusnaApp());
 
 class AsmsHusnaApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -65,6 +69,19 @@ class AsmsHusnaApp extends StatelessWidget {
         primaryColor: Colors.lightBlue[900],
         accentColor: Colors.lightBlue[700],
       ),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ar', 'DZ'), // English
+        const Locale('en', 'US'), // English
+        const Locale('de', 'DE'), // German
+        const Locale('fr', 'FR'), // German
+        // ... other locales the app supports
+      ],
       home: NamesPage(title: 'Asma-Lahi-l-Husna'),
     );
   }
@@ -74,7 +91,7 @@ class NamesPage extends StatefulWidget {
   NamesPage({Key key, this.title}) : super(key: key);
 
   final String title;
-
+  List<Name> _nameForDisplay = List<Name>();
   @override
   _NamesPageState createState() => _NamesPageState();
 }
@@ -82,7 +99,10 @@ class NamesPage extends StatefulWidget {
 class _NamesPageState extends State<NamesPage> {
   Future<Names> futureNames;
   final List<Name> _saved = new List<Name>();
+
   //final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  static AudioCache player = AudioCache();
 
   @override
   void initState() {
@@ -98,6 +118,19 @@ class _NamesPageState extends State<NamesPage> {
         primaryColor: Colors.lightBlue[900],
         accentColor: Colors.lightBlue[700],
       ),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ar', 'DZ'), // English
+        const Locale('en', 'US'), // English
+        const Locale('de', 'DE'), // German
+        const Locale('fr', 'FR'), // German
+        // ... other locales the app supports
+      ],
       home: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -111,8 +144,23 @@ class _NamesPageState extends State<NamesPage> {
           ),
           actions: <Widget>[
             // Add 3 lines from here...
+            new IconButton(
+              onPressed: () {
+                //showSearch(context: context, delegate: Search(widget._nameForDisplay));
+                //player.play("audio/all.mp3");
+              },
+              icon: Icon(Icons.play_circle_fill),
+            ),
+            new IconButton(
+              onPressed: () {
+                showSearch(context: context, delegate: Search(widget._nameForDisplay));
+              },
+              icon: Icon(Icons.search),
+            ),
             new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+
           ],
+          centerTitle: true,
         ),
         body: Center(
           child: FutureBuilder<Names>(
@@ -124,7 +172,6 @@ class _NamesPageState extends State<NamesPage> {
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
-
               // By default, show a loading spinner.
               return CircularProgressIndicator();
             },
@@ -134,10 +181,12 @@ class _NamesPageState extends State<NamesPage> {
     );
   }
 
+
   void _pushSaved() {
     Navigator.of(context).push(
       new MaterialPageRoute<void>(
         builder: (BuildContext context) {
+          Locale myLocale = Localizations.localeOf(context);
           final Iterable<ListTile> tiles = _saved.map(
             (Name name) {
               return new ListTile(
@@ -150,21 +199,44 @@ class _NamesPageState extends State<NamesPage> {
                     ),
                   ),
                 ),
-                title: Text(
-                  name.arabic,
-                  //style: TextStyle(fontWeight: FontWeight.bold),
-                  style: new TextStyle(
-                    fontFamily: ArabicFonts.Amiri,
-                    fontWeight: FontWeight.bold,
-                    package: 'google_fonts_arabic',
-                    fontSize: 25.0,
-                  ),
+                title: Row(
+                  children: <Widget>[
+                    (name.number == "00" || name.number == "85")
+                        ? Text(
+                            name.arabic,
+                            //style: TextStyle(fontWeight: FontWeight.bold),
+                            style: new TextStyle(
+                              fontFamily: ArabicFonts.Amiri,
+                              fontWeight: FontWeight.bold,
+                              package: 'google_fonts_arabic',
+                              fontSize: 1.0,
+                            ),
+                          )
+                        : Text(name.transliteration),
+                    Spacer(flex: 1),
+                    Text(
+                      name.arabic,
+                      //style: TextStyle(fontWeight: FontWeight.bold),
+                      style: new TextStyle(
+                        fontFamily: ArabicFonts.Amiri,
+                        fontWeight: FontWeight.bold,
+                        package: 'google_fonts_arabic',
+                        fontSize: 25.0,
+                      ),
+                    )
+                  ],
                 ),
-                subtitle: Text(name.transliteration),
-                /*title: new Text(
-                  name.arabic,
-                  style: _biggerFont,
-                ),*/
+
+                subtitle: Text(name.meaning[myLocale.languageCode]),
+
+                trailing: Icon(
+                  Icons.play_arrow,
+                ),
+                onTap: () {
+                  // you can add Play/push code over here
+                  print("audio/" + name.number + ".mp3");
+                  player.play("audio/" + name.number + ".mp3");
+                },
               );
             },
           );
@@ -184,7 +256,9 @@ class _NamesPageState extends State<NamesPage> {
   }
 
   Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
+    Locale myLocale = Localizations.localeOf(context);
     Names values = snapshot.data;
+    widget._nameForDisplay.addAll(values.names);
     return new ListView.builder(
       itemCount: values.names.length,
       itemBuilder: (BuildContext context, int index) {
@@ -203,23 +277,36 @@ class _NamesPageState extends State<NamesPage> {
                     ),
                   ),
                 ),
-                title: Text(
-                  values.names[index].arabic,
-                  //style: TextStyle(fontWeight: FontWeight.bold),
-                  style: new TextStyle(
-                    fontFamily: ArabicFonts.Amiri,
-                    fontWeight: FontWeight.bold,
-                    package: 'google_fonts_arabic',
-                    fontSize: 25.0,
-                  ),
-                ),
-                subtitle: Text(values.names[index]
-                        .transliteration /* +
-                    "\n" +
-                    "" +
-                    "\n" +
-                    values.names[index].meaning["de"]*/
+                title: Row(
+                  children: <Widget>[
+                    (values.names[index].number == "00" ||
+                            values.names[index].number == "85")
+                        ? (Text(
+                            values.names[index].arabic,
+                            //style: TextStyle(fontWeight: FontWeight.bold),
+                            style: new TextStyle(
+                              fontFamily: ArabicFonts.Amiri,
+                              fontWeight: FontWeight.bold,
+                              package: 'google_fonts_arabic',
+                              fontSize: 1.0,
+                            ),
+                          ))
+                        : Text(values.names[index].transliteration),
+                    Spacer(flex: 1),
+                    Text(
+                      values.names[index].arabic,
+                      //style: TextStyle(fontWeight: FontWeight.bold),
+                      style: new TextStyle(
+                        fontFamily: ArabicFonts.Amiri,
+                        fontWeight: FontWeight.bold,
+                        package: 'google_fonts_arabic',
+                        fontSize: 25.0,
+                      ),
                     ),
+                  ],
+                ),
+                subtitle:
+                    Text(values.names[index].meaning[myLocale.languageCode]),
                 //trailing: Icon(Icons.more_vert),
                 trailing: new Icon(
                   alreadySaved ? Icons.check_box : Icons.check,
@@ -246,3 +333,160 @@ class _NamesPageState extends State<NamesPage> {
     );
   }
 }
+
+class Search extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  Name selectedResult = null;
+
+  @override
+  Widget buildResults(BuildContext context) {
+    Locale myLocale = Localizations.localeOf(context);
+    return Container(
+        child:        Card(
+          child: ListTile(
+            leading: CircleAvatar(
+              //backgroundColor: Colors.lightBlue,
+              child: Text(
+                selectedResult.number,
+                style: new TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            title: Row(
+              children: <Widget>[
+                (selectedResult.number == "00" ||
+                    selectedResult.number == "85")
+                    ? (Text(
+                  selectedResult.arabic,
+                  //style: TextStyle(fontWeight: FontWeight.bold),
+                  style: new TextStyle(
+                    fontFamily: ArabicFonts.Amiri,
+                    fontWeight: FontWeight.bold,
+                    package: 'google_fonts_arabic',
+                    fontSize: 1.0,
+                  ),
+                ))
+                    : Text(selectedResult.transliteration),
+                Spacer(flex: 1),
+                Text(
+                  selectedResult.arabic,
+                  //style: TextStyle(fontWeight: FontWeight.bold),
+                  style: new TextStyle(
+                    fontFamily: ArabicFonts.Amiri,
+                    fontWeight: FontWeight.bold,
+                    package: 'google_fonts_arabic',
+                    fontSize: 25.0,
+                  ),
+                ),
+              ],
+            ),
+            subtitle:
+            Text(selectedResult.meaning[myLocale.languageCode]),
+            //trailing: Icon(Icons.more_vert),
+
+          ),
+        ),
+    );
+  }
+
+  List<Name> listExample;
+  Search(this.listExample);
+
+  List<Name> recentList = new List<Name>();
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    //recentList.addAll(listExample);
+    List<Name> suggestionList = new List<Name>();
+    query.isEmpty
+        ? suggestionList = recentList //In the true case
+        : suggestionList.addAll(listExample.where(
+            // In the false case
+            (element) => element.number.contains(query),
+          ));
+    /*List<Name> suggestionList = query.isEmpty
+        ? listExample
+        : listExample
+        .where((x) => x.number.startsWith(query))
+        .toList();*/
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        Locale myLocale = Localizations.localeOf(context);
+       /* return ListTile(
+          title: Text(
+            suggestionList[index].number,
+          ),
+         // leading: query.isEmpty ? Icon(Icons.access_time) : SizedBox(),*/
+        return new ListTile(
+          leading: CircleAvatar(
+            //backgroundColor: Colors.lightBlue,
+            child: Text(
+              suggestionList[index].number,
+              style: new TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          title: Row(
+            children: <Widget>[
+              (suggestionList[index].number == "00" || suggestionList[index].number == "85")
+                  ? Text(
+                suggestionList[index].arabic,
+                //style: TextStyle(fontWeight: FontWeight.bold),
+                style: new TextStyle(
+                  fontFamily: ArabicFonts.Amiri,
+                  fontWeight: FontWeight.bold,
+                  package: 'google_fonts_arabic',
+                  fontSize: 1.0,
+                ),
+              )
+                  : Text(suggestionList[index].transliteration),
+              Spacer(flex: 1),
+              Text(
+                suggestionList[index].arabic,
+                //style: TextStyle(fontWeight: FontWeight.bold),
+                style: new TextStyle(
+                  fontFamily: ArabicFonts.Amiri,
+                  fontWeight: FontWeight.bold,
+                  package: 'google_fonts_arabic',
+                  fontSize: 25.0,
+                ),
+              )
+            ],
+          ),
+
+          subtitle: Text(suggestionList[index].meaning[myLocale.languageCode]),
+          onTap: () {
+            selectedResult = suggestionList[index];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+}
+
